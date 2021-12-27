b74679fa8aac159100e29c0d595208037fba558b
kirincastle <60503208+kirincastle@users.noreply.github.com>
2021-12-27 23:34:12 +0800

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,5 +1,8 @@
+read -p "Set to true if you're testing your setup, set to false if production: " staging
+sed 's+staging_arg:.*+staging_arg: "'$staging'"+g' ./docker-compose.yml | sponge ./docker-compose.yml
+
 read -p "your domain is: " yourdomain
-sed "s/domains=(.*)/domains=($yourdomain)/g" ./init-letsencrypt.sh | sponge ./init-letsencrypt.sh
+sed 's+domain_args:.*+domain_args: '$yourdomain'+g' ./docker-compose.yml | sponge ./docker-compose.yml
 sed 's+"Host": ".*"+"Host": "'"$yourdomain"'"+g' ./data/v2ray/config.json | sponge ./data/v2ray/config.json
 sed "s/server_name .*;/server_name $yourdomain;/g" ./data/nginx/conf.d/v2ray.conf | sponge ./data/nginx/conf.d/v2ray.conf
 sed "s+https://.*$request_uri;+https://$yourdomain\$request_uri;+g" ./data/nginx/conf.d/v2ray.conf | sponge ./data/nginx/conf.d/v2ray.conf
@@ -7,10 +10,10 @@ sed "s+/etc/letsencrypt/live/.*/+/etc/letsencrypt/live/$yourdomain/+g" ./data/ng
 sed "s+tls:tls -a .* -m ps:.*+tls:tls -a $yourdomain -m ps:$yourdomain --debug ./data/v2ray/config.json+g" ./init-letsencrypt.sh | sponge ./init-letsencrypt.sh
 
 read -p "your email for certificate registration is: " email
-sed 's+email=".*"+email="'"$email"'"+g' ./init-letsencrypt.sh | sponge ./init-letsencrypt.sh
+sed 's+email_arg:.*+email_arg: '$email'+g' ./docker-compose.yml | sponge ./docker-compose.yml
 
-read -p "Set to 1 if you're testing your setup, set to 0 if production: " staging
-sed "s/staging=0/staging=$staging/g" ./init-letsencrypt.sh | sponge ./init-letsencrypt.sh
+read -p "Authen token for cloudflare: " authtoken
+sed 's+auth_token:.*+auth_token: '$authtoken'+g' ./docker-compose.yml | sponge ./docker-compose.yml
 
 read -p "What is the v2ray web socket path? e.g. please input /abcpath (https://www.uuidgenerator.net/version1) : " path
 sed 's+"path": ".*",+"path": "'"$path"'",+g' ./data/v2ray/config.json | sponge ./data/v2ray/config.json
